57246a5c34ecdaf56103bc6d64cd010ce4b04d7f
Valeria Torgonina <lovec.cnov@mail.ru>
2019-12-03 20:28:39 +0300

    create user-list

--- src/modules/main/components/table.js
+++ src/modules/main/components/table.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import icon from '../../../images/card-bg.jpg';
 
 class UserTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            url: ''
+        }
+    }
 
     render() {
         return (
@@ -15,45 +20,37 @@ class UserTable extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>1</td>
-                            <td>
-                                <div 
-                                    style={{
-                                        width: '70px', 
-                                        height: '50px', 
-                                        background: `url(${icon}) 50% 50% no-repeat`, 
-                                        backgroundSize: 'cover', 
-                                        margin: '0 auto'
-                                    }}>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Jacob</td>
-                            <td>1</td>
-                            <td>
-                                <div className="input-group mb-3" style={{width: '300px', textAlign: 'start', margin: '0 auto'}}>
-                                    <div className="custom-file">
-                                        <input type="file" className="custom-file-input" id="avatar-upload"/>
-                                        <label className="custom-file-label" for="avatar-upload">Выберите файл</label>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Larry</td>
-                            <td>1</td>
-                            <td>
-                                <div className="input-group mb-3" style={{width: '300px', textAlign: 'start', margin: '0 auto'}}>
-                                    <div className="custom-file">
-                                        <input type="file" className="custom-file-input" id="avatar-upload"/>
-                                        <label className="custom-file-label" for="avatar-upload">Выберите файл</label>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
+                        {this.props.usersList.map(({avatarUrl, id, name}) => {
+                            return (
+                                <tr key={id}>
+                                    <td>{name}</td>
+                                    <td>{id}</td>   
+                                    <td>{!avatarUrl ? 
+                                        (<div className="input-group mb-3" style={{width: '300px', textAlign: 'start', margin: '0 auto'}}>
+                                            <div className="custom-file">
+                                                <input 
+                                                    type="file" 
+                                                    name='avatarUrl' 
+                                                    className="custom-file-input" 
+                                                    id="avatar-upload"
+                                                    onChange={(e) => this.props.selectAvatar(e.target.files[0], id)}
+                                                />
+                                                <label className="custom-file-label" htmlFor="avatar-upload">выберите файл</label>
+                                            </div>
+                                        </div>) :
+                                        (<div style={{
+                                            width: '70px', 
+                                            height: '50px', 
+                                            background: `url(${avatarUrl}) 50% 50% no-repeat`, 
+                                            backgroundSize: 'cover', 
+                                            margin: '0 auto'
+                                        }}> 
+                                        </div>)
+
+                                    }</td>   
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
         </div>
